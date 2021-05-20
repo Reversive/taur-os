55174e81efb111067f432f05e291a36fd4d4b713
lulydk <lucidiaz@itba.edu.ar>
2021-05-20 08:12:43 -0300

    cleaning + comments

--- Kernel/mem/include/buddy.h
+++ Kernel/mem/include/buddy.h
@@ -1,14 +1,21 @@
 #ifndef _MM_H
 #define _MM_H
 
-    // #include <stdio.h>
     #include <string.h>
     #include <stdint.h>
     #include "sbrk.h"
 
+    #define BASE 2
+    #define GIGA 30
+    #define MIN_ALLOC_LOG2 6                            // Min size = 64 bytes
+    #define MAX_ALLOC_LOG2 (GIGA - MIN_ALLOC_LOG2)      // Max size = 1 GB
+    #define BINARY_POWER(x) (1 << (x))
+    #define TOTAL_HEAP_SIZE (1024 * 500)
+
     #ifndef NULL
         #define NULL 0
     #endif
+    
     typedef enum { false, true } bool;
 
     void free(void *ptr);
--- Kernel/mem/sbrk.c
+++ Kernel/mem/sbrk.c
@@ -1,20 +1,18 @@
 // // This is a personal academic project. Dear PVS-Studio, please check it.
 // // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-#include "include/sbrk.h"
-#include <stdlib.h>
 
-/***************************************************************/
-/*                        Variables                            */
-/***************************************************************/
+#include "include/sbrk.h"
+
+//---------------------------DECLARACION DE VARIABLES--------------------------------------//
+
 static void *const sampleDataModuleAddress = (void *)0x600000; 
 static void *const maxAddress = (void *)0xF00000;             
 void *topAddress = NULL;
 
-/***************************************************************/
-/*                   Funciones Publicas                        */
-/***************************************************************/
+//-----------------------------------------------------------------------------------------//
 
-void sbrk_handler(int increment, void **buffer)
+
+void sbrkHandler(int increment, void **buffer)
 {
         if (topAddress == NULL)
         {
@@ -26,23 +24,21 @@ void sbrk_handler(int increment, void **buffer)
 
                 *buffer = topAddress;
                 topAddress += increment;
-        }
-        else
+        } else
         {
                 *buffer = NULL;
         }
         return;
 }
+
 int brk_handler(void *addr)
 {
         if (topAddress == NULL)
         {
                 topAddress = sampleDataModuleAddress;
         }
-        // No puede reservar mas memoria que la que tiene asignada el proceso, o
-        // un valor menor al que tiene ya asignado
-        if (addr > maxAddress || addr < topAddress)
-        {
+        if (addr > maxAddress || addr < topAddress)     // Can't have more memory than the process
+        {                                               // nor less than the already assigned value
                 return -1;
         }
 
